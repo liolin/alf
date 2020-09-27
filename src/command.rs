bbca276c352a5547da0d111368dcb08e41c61f53
Olivier Lischer <olivier.lischer@liolin.ch>
2020-09-27 09:35:17 +0200

    move commands to own file

---
+++ src/command.rs
@@ -0,0 +1,52 @@
+use std::env;
+use std::collections::HashMap;
+
+use crate::Bookshelf;
+
+pub trait Command {
+    fn run(self);
+    fn with_arguments(args: HashMap<String, String>) -> Self;
+}
+
+pub struct List {
+    args: HashMap<String, String>
+}
+
+pub struct Open {
+    args: HashMap<String, String>
+}
+
+impl Command for List {
+    fn run(self) {
+        let home = env::var("HOME").unwrap();
+        let bookshelf = Bookshelf::from_file(format!("{}/.alf.toml", home).as_str());
+        for bookmark in bookshelf.bookmarks {
+            println!("{}:\n\t{}", bookmark.name, bookmark.url);
+        }
+    }
+
+    fn with_arguments(args: HashMap<String, String>) -> Self {
+        Self {
+            args
+        }
+    }
+}
+
+impl Command for Open {
+    fn run(self) {
+        let home = env::var("HOME").unwrap();
+        let bookshelf = Bookshelf::from_file(format!("{}/.alf.toml", home).as_str());
+        if let Some(bookmark) = bookshelf.find_by_name(self.args.get("name").unwrap().as_str()) {
+            if webbrowser::open(bookmark.url.as_str()).is_ok() {
+
+            }
+        }
+
+    }
+
+    fn with_arguments(args: HashMap<String, String>) -> Self {
+        Self {
+            args
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,23 @@
 // read all bookmarks from a storage and present as a search engine
 // alf list # lists all bookmarks
 // alf open DuckDuckGo # opens url with name DuckDuckGo
-use serde::{Serialize, Deserialize};
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, SubCommand};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::env;
 
-trait Command {
-    fn run(self);
-    fn with_arguments(args: HashMap<String, String>) -> Self;
-}
+mod command;
+use command::Command;
 
-struct List {
-    args: HashMap<String, String>
-}
-
-struct Open {
-    args: HashMap<String, String>
-}
-
-impl Command for List {
-    fn run(self) {
-        let home = env::var("HOME").unwrap();
-        let bookshelf = Bookshelf::from_file(format!("{}/.alf.toml", home).as_str());
-        for bookmark in bookshelf.bookmarks {
-            println!("{}:\n\t{}", bookmark.name, bookmark.url);
-        }
-    }
-
-    fn with_arguments(args: HashMap<String, String>) -> Self {
-        Self {
-            args
-        }
-    }
-}
-
-impl Command for Open {
-    fn run(self) {
-
-        let home = env::var("HOME").unwrap();
-        let bookshelf = Bookshelf::from_file(format!("{}/.alf.toml", home).as_str());
-        if let Some(bookmark) = bookshelf.find_by_name(self.args.get("name").unwrap().as_str()) {
-            if webbrowser::open(bookmark.url.as_str()).is_ok() {
-
-            }
-        }
-
-    }
-
-    fn with_arguments(args: HashMap<String, String>) -> Self {
-        Self {
-            args
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
 struct Bookmark {
     name: String,
     url: String,
-    tags: Option<Vec<String>>,
+    tags: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 struct Bookshelf {
-    bookmarks: Vec<Bookmark>
+    bookmarks: Vec<Bookmark>,
 }
 
 impl Bookshelf {
@@ -79,60 +32,90 @@ impl Bookshelf {
         bookshelf
     }
 
-    pub fn find_by_name(self, name: &str) -> Option<Bookmark> {
-        for bookmark in self.bookmarks {
-            if bookmark.name.to_lowercase() == name.to_string().to_lowercase() {
-                return Some(bookmark)
+    pub fn find_by_name(&self, name: &str) -> Option<&Bookmark> {
+        for bookmark in &self.bookmarks {
+            if bookmark.name.to_lowercase() == name.to_lowercase() {
+                return Some(&bookmark)
             }
         }
         None
     }
+
+    // pub fn find_by_tag(self, tag: &str) -> Option<Vec<&Bookmark>> {
+    //     // let vec: &Vec<_>= &self
+    //     //     .bookmarks
+    //     //     .into_iter()
+    //     //     .filter(|x| x.clone().has_tag(tag))
+    //     //     .collect();
+    //     let mut vec = Vec::new();
+    //     for bookmark in &self.bookmarks {
+    //         if bookmark.has_tag(tag) {
+    //             vec.push(bookmark);
+    //         }
+    //     }
+
+    //     if vec.len() == 0 {
+    //         return None;
+    //     }
+
+    //     Some(vec)
+    // }
 }
 
 impl Bookmark {
-    pub fn new(name: &str, url: &str, tags: Option<Vec<String>>) -> Bookmark {
+    pub fn new(name: &str, url: &str, tags: Vec<String>) -> Bookmark {
         Bookmark {
             name: name.to_string(),
             url: url.to_string(),
-            tags
+            tags,
         }
+    }
+
+    pub fn has_tag(&self, tag: &str) -> bool {
+        self.tags.contains(&tag.to_string())
     }
 }
 
 fn main() {
     let matches = App::new("Alf")
-                          .version("0.1.0")
-                          .author("Olivier Lischer <olivier.lischer@liolin.ch>")
-                          .about("Access your bookmarks from the CLI")
-                          .arg(Arg::with_name("store")
-                               .short("s")
-                               .value_name("STORE")
-                               .takes_value(true))
-                          .subcommand(SubCommand::with_name("list"))
-                          .subcommand(SubCommand::with_name("open")
-                                      .arg(Arg::with_name("name")
-                                           .short("n")
-                                           .value_name("NAME")
-                                           .takes_value(true)
-                                           .required(true)
-                                           .help("print debug information verbosely"))).get_matches();
+        .version("0.1.0")
+        .author("Olivier Lischer <olivier.lischer@liolin.ch>")
+        .about("Access your bookmarks from the CLI")
+        .arg(
+            Arg::with_name("store")
+                .short("s")
+                .value_name("STORE")
+                .takes_value(true),
+        )
+        .subcommand(SubCommand::with_name("list"))
+        .subcommand(
+            SubCommand::with_name("open").arg(
+                Arg::with_name("name")
+                    .short("n")
+                    .value_name("NAME")
+                    .takes_value(true)
+                    .required(true)
+                    .help("print debug information verbosely"),
+            ),
+        )
+        .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("open") {
         let x = matches.value_of("name").unwrap();
         let mut map = HashMap::new();
         map.insert("name".to_string(), x.to_string());
-        Open::with_arguments(map).run();
+        command::Open::with_arguments(map).run();
     } else {
-        List::with_arguments(HashMap::new()).run();
+        command::List::with_arguments(HashMap::new()).run();
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use rand::prelude::*;
     use std::fs::File;
     use std::io::prelude::*;
-    use rand::prelude::*;
 
     fn create_tmp_file(content: &str) -> (File, String) {
         let mut name = String::new();
@@ -147,12 +130,12 @@ mod test {
         file.write_all(content.as_bytes()).unwrap();
         (file, name)
     }
-    
+
     #[test]
     fn test_parse_toml_file_to_bookshelf() {
         let toml_string = r#"
 bookmarks = [
-    { name = "DuckDuckGo", url = "https://www.duckduckgo.com" },
+    { name = "DuckDuckGo", url = "https://www.duckduckgo.com", tags = []},
     { name = "Google", url = "https://www.google.com", tags = ["search engine"] }
 ]
 "#;
@@ -163,16 +146,60 @@ bookmarks = [
                 Bookmark {
                     name: "DuckDuckGo".to_string(),
                     url: "https://www.duckduckgo.com".to_string(),
-                    tags: None,
+                    tags: Vec::new(),
                 },
                 Bookmark {
                     name: "Google".to_string(),
                     url: "https://www.google.com".to_string(),
-                    tags: Some(vec!["search engine".to_string()]),
+                    tags: vec!["search engine".to_string()],
                 },
-            ]
+            ],
         };
         let rhs = Bookshelf::from_file(path.as_str());
-        assert_eq!(lhs, rhs)
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_find_by_name() {
+        let google = Bookmark {
+            name: "Google".to_string(),
+            url: "https://www.google.com".to_string(),
+            tags: vec!["search engine".to_string()],
+        };
+        let bookshelf = Bookshelf {
+            bookmarks: vec![
+                Bookmark {
+                    name: "DuckDuckGo".to_string(),
+                    url: "https://www.duckduckgo.com".to_string(),
+                    tags: Vec::new(),
+                },
+                google.clone()
+            ],
+        };
+
+        let rhs = bookshelf.find_by_name("Google");
+        assert_eq!(Some(&google), rhs);
+        let rhs = bookshelf.find_by_name("google");
+        assert_eq!(Some(&google.clone()), rhs);
+        assert_eq!(None, bookshelf.find_by_name("i am not here"));
+    }
+
+    #[test]
+    fn test_bookmark_has_tag() {
+        let google = Bookmark {
+            name: "Google".to_string(),
+            url: "https://www.google.com".to_string(),
+            tags: vec!["search engine".to_string()],
+        };
+
+        let empty = Bookmark {
+            name: "Google".to_string(),
+            url: "https://www.google.com".to_string(),
+            tags: Vec::new(),
+        };
+
+        assert_eq!(true, google.has_tag("search engine"));
+        assert_eq!(false, google.has_tag("i am not here"));
+        assert_eq!(false, empty.has_tag("i am not here"));
     }
 }
